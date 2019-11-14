db49e2cf93d300242a97a13ac2f0dfaec9c7e9b7
jtj0002 <jtj0002@mix.wvu.edu>
2019-11-14 13:05:17 -0500

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+var page = 'home';
+
+/* This javascript file should allow the hovering over
+    and interaction with certain aspects of the website.
+    like being able to change the background depending on
+    where the mouse is as well as change the layout depending
+    on the size of the screen.
+*/
+
