75af77dbd09648e5ab4a7d2c537218f513863e13
jtj0002 <jtj0002@mix.wvu.edu>
2020-01-27 18:02:19 -0500

    Bootstrap base files needed to run the webpage

--- assets/js/app.js
+++
@@ -1,9 +0,0 @@
-var page = 'home';
-
-/* This javascript file should allow the hovering over
-    and interaction with certain aspects of the website.
-    like being able to change the background depending on
-    where the mouse is as well as change the layout depending
-    on the size of the screen.
-*/
-
